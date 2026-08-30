874980ddd255e93cc91e21143a9023f0e49f0597
Alberth Ramirez <albert.2584@hotmail.com>
2026-08-29 19:39:54 -0500

    Estabilización test v2

--- playwright.admin.config.ts
+++ playwright.admin.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
   webServer: {
-    command: 'npm.cmd run dev -- --host 127.0.0.1 --port 5174',
+    command: `${process.platform === 'win32' ? 'npm.cmd' : 'npm'} run dev -- --host 127.0.0.1 --port 5174`,
     url: 'http://127.0.0.1:5174',
     reuseExistingServer: false,
     env: {
--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
   webServer: {
-    command: 'npm.cmd run dev -- --host 127.0.0.1',
+    command: `${process.platform === 'win32' ? 'npm.cmd' : 'npm'} run dev -- --host 127.0.0.1`,
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
     env: {
